623f26b0a9699a3fab676641bdc655d799726347
bcmbf14 <bcmbf14@gmail.com>
2020-04-10 01:31:29 +0900

    Initial Commit

---
+++ Bcmbf14Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bcmbf14Memo
+//
+//  Created by jongchankim on 2020/04/10.
+//  Copyright © 2020 jongchankim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
