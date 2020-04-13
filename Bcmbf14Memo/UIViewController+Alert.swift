0512dcac347752fc6451832a3d86c8109621cc6c
bcmbf14 <bcmbf14@gmail.com>
2020-04-14 04:46:51 +0900

    메모저장

---
+++ Bcmbf14Memo/UIViewController+Alert.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController+Alert.swift
+//  Bcmbf14Memo
+//
+//  Created by jongchankim on 2020/04/14.
+//  Copyright © 2020 jongchankim. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func alert(title: String = "알림", message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+}
+
